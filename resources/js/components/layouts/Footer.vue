ed4f8187718c1b38a957d58dd5c69b5ce3e50f0b
Lissan Haider <lissanh95@hotmail.com>
2018-10-19 21:40:54 +0500

    footer compelet

--- resources/js/components/layouts/Footer.vue
+++ resources/js/components/layouts/Footer.vue
@@ -16,36 +16,45 @@
                 </div>
                 <div class="col-3 contact">
                     <div class="this-is-us">
-                        <div class="footer-heading">THIS IS US</div>
+                        <div class="footer-heading">TELL US YOUR IDEA</div>
                         <div class="footer-para">There are a lot of companies that do what we do. They share the same what and how, but our clients work with us for our why and our who. We're IndiaNIC, and you're in the right place.</div>
                     </div>
                     <div class="footer-bottom">
-                        <div class="heading-bold">ABOUT</div>
+                        <div class="heading-bold">CONTACT</div>
                         <div class="para">Creative content studio based in jersey City, Pakistan</div>
                     </div>
                     <div class="background"></div>
                 </div>
                 <div class="col-3 portfolio">
                     <div class="this-is-us">
-                        <div class="footer-heading">THIS IS US</div>
+                        <div class="footer-heading">STUFF WE DID</div>
                         <div class="footer-para">There are a lot of companies that do what we do. They share the same what and how, but our clients work with us for our why and our who. We're IndiaNIC, and you're in the right place.</div>
                     </div>
                     <div class="footer-bottom">
-                        <div class="heading-bold">ABOUT</div>
+                        <div class="icons">
+                            <i class="fab fa-facebook-f"></i>
+                            <i class="fab fa-instagram"></i>
+                            <i class="fab fa-twitter"></i>
+                            <i class="fab fa-pinterest-p"></i>
+                            <i class="fab fa-behance"></i>
+                        </div>
                         <div class="para">Creative content studio based in jersey City, Pakistan</div>
                     </div>
                     <div class="background"></div>
                 </div>
                 <div class="col-3 links">
                     <div class="this-is-us">
-                        <div class="footer-heading">THIS IS US</div>
-                        <div class="footer-para">There are a lot of companies that do what we do. They share the same what and how, but our clients work with us for our why and our who. We're IndiaNIC, and you're in the right place.</div>
+                        <div class="footer-heading" style="visibility: hidden;">LINKS</div>
+                        <div class="footer-para">Home</div>
+                        <div class="footer-para">Design</div>
+                        <div class="footer-para">Development</div>
+                        <div class="footer-para">Legal Terms</div>
+                        <div class="footer-para">Cookie Policy</div>
                     </div>
                     <div class="footer-bottom">
-                        <div class="heading-bold">ABOUT</div>
+                        <div class="heading-bold" style="visibility: hidden;">LINKS</div>
                         <div class="para">Creative content studio based in jersey City, Pakistan</div>
                     </div>
-                    <div class="background"></div>
                 </div>
             </div>
         </div>
@@ -97,6 +106,14 @@
         bottom: 0;
         padding-bottom:1rem;
         padding-right:1rem;
+        .icons{
+            color: $white;
+            font-size: 1.5rem;
+            padding-bottom: 0.8rem;
+            i{
+                padding-right: 0.5rem;
+            }
+        }
         .heading-bold{
             font-size: 3.5rem;
             text-align: left;
@@ -106,6 +123,7 @@
         }
     }.background{
         position: absolute;
+        z-index: -1;
         bottom:0;
         width: 100%;
         height: 150%;
