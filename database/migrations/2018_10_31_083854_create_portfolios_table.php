14b67ffe16e1a1c642ffd3821856d6ddf29cfc04
Lissan Haider <lissanh95@hotmail.com>
2018-10-31 13:55:51 +0500

    created model and tables

---
+++ database/migrations/2018_10_31_083854_create_portfolios_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePortfoliosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('portfolios', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->string('category');
+            $table->integer('year');
+            $table->string('colors');
+            $table->string('img-cover');
+            $table->string('images');
+            $table->string('tags');
+            $table->string('discription');
+            $table->string('website-link');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('portfolios');
+    }
+}
